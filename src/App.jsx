092171adf79971e3e8af0f19b5338bdf446c030e
katovarcon <k.tovarcontreras@hotmail.com>
2026-03-26 17:42:50 -0600

    feat: create base components structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Form from "./components/Form/Form";
+import TodoList from "./components/TodoList/TodoList";
+
 function App() {
   return (
     <div>
       <h1>Todo App</h1>
+      <Form />
+      <TodoList />
     </div>
   );
 }
